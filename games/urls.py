662b598dbca2328a1bbc016467ee0fa74ba066cd
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-29 11:23:09 -0800

    update URL definitions

--- games/urls.py
+++ games/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = [
-    url(r'^games$', views.games_list, name='games_list'),
-    url(r'^games/create$', views.game_create, name='game_create'),
+    # games API Urls
+    url(r'^games/?$', views.games_list, name='games_list'),
+    url(r'^games/create/?$', views.game_create, name='game_create'),
 
-    url(r'^platforms$', views.platforms_list, name='platforms_list'),
-    url(r'^platforms/create$', views.platform_create, name='platform_create'),
+    # platforms API URLs
+    url(r'^platforms/?$', views.platforms_list, name='platforms_list'),
+    url(r'^platforms/(?P<id>\d+)/?$', views.platform_detail, name='platform_detail'),
+    url(r'^platforms/create/?$', views.platform_create, name='platform_create'),
 ]
