84b1cf5539f1f8abbb6537d4ca85f48698164320
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-30 12:58:10 -0800

    added @ensure_csrf_cookie decorator to main view

--- game_on/views.py
+++ game_on/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
