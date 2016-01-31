962ede9c80b5c041616c4e0cf3ccc9a91fdac38c
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-31 14:06:24 -0800

    login requirements added to main view

--- game_on/urls.py
+++ game_on/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/', include('games.urls', namespace='api')),
     url(r'^admin/', include(admin.site.urls)),
+
+    # login/logout
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
--- game_on/views.py
+++ game_on/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@login_required
 @ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
