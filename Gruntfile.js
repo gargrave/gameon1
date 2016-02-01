ca6fd67ac1981a975e3a9e335d30499980f2e5b2
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-31 16:39:08 -0800

    add 'build' Grunt task to gulify scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,37 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     config: {
-      title: 'game_on'
+      title: 'GameOn',
+      date: function() {
+        var d = new Date();
+        return d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
+      }
+    },
+
+    /*=============================================
+     = uglify
+     =============================================*/
+    uglify: {
+      main: {
+        options: {
+          mangle: false,
+          banner: '/* <%= config.title %> | built on <%= config.date() %> */'
+        },
+        files: [{
+          'game_on/static/app/app.min.js': [
+            'game_on/static/app/app.js',
+            'game_on/static/app/**/*.module.js',
+            'game_on/static/app/**/*.ctrl.js',
+            'game_on/static/app/**/*.svc.js',
+            'game_on/static/app/**/*.menus.js',
+            'game_on/static/app/**/*.dir.js'
+          ]
+        }]
+      }
     }
 
   });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('build', ['uglify']);
 };
