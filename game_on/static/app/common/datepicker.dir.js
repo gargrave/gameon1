9c32b313263336cc635b1c7b5e4bf32a7f184fd8
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-31 23:03:03 -0800

    integrate datepicker

---
+++ game_on/static/app/common/datepicker.dir.js
@@ -0,0 +1,27 @@
+var App;
+(function (App) {
+    var Platforms;
+    (function (Platforms) {
+        var counter = 0;
+        angular.module('gameon').directive('datepicker', [
+            function () {
+                return {
+                    restrict: 'AE',
+                    scope: {
+                        label: '@',
+                        dateModel: '='
+                    },
+                    template: "\n        <label for=\"{A ::uniqueId A}\"><span ng-bind=\"label\"></span></label>\n        <div id=\"{A ::uniqueId A}\">\n          <input type=\"text\" class=\"form-control\" maxlength=\"10\"\n                 ng-model=\"dateModel\" required>\n        </div>\n        ",
+                    link: function ($scope, elem) {
+                        $scope.uniqueId = 'datepicker_' + counter++;
+                        elem.find('input').datepicker({
+                            format: 'yyyy-mm-dd',
+                            autoclose: true,
+                            todayHighlight: true
+                        });
+                    }
+                };
+            }
+        ]);
+    })(Platforms = App.Platforms || (App.Platforms = {}));
+})(App || (App = {}));
---
+++ game_on/static/app/common/datepicker.dir.ts
@@ -0,0 +1,37 @@
+module App.Platforms {
+  // a counter to give unique names to each instance
+  let counter = 0;
+
+  angular.module('gameon').directive('datepicker', [
+
+    function() {
+      return {
+        restrict: 'AE',
+        scope: {
+          label: '@',
+          dateModel: '='
+        },
+
+        template: `
+        <label for="{A ::uniqueId A}"><span ng-bind="label"></span></label>
+        <div id="{A ::uniqueId A}">
+          <input type="text" class="form-control" maxlength="10"
+                 ng-model="dateModel" required>
+        </div>
+        `,
+
+        link: function($scope, elem) {
+          // set a unique id for each instance
+          $scope.uniqueId = 'datepicker_' + counter++;
+
+          // set up datepicker options
+          elem.find('input').datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            todayHighlight: true
+          });
+        }
+      };
+    }
+  ]);
+}
