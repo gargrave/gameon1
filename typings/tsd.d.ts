5d314b30a022a2016396d6598a1c784cf87d9573
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-01-31 21:43:16 -0800

    add angular-animate lib

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -5,3 +5,4 @@
 /// <reference path="angularjs/angular-mocks.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angularjs/angular-animate.d.ts" />
